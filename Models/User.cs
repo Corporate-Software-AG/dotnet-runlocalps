9412943da307a7e9e4beceaaa28728c4cd8ca1f3
Thomas <65050898+tvonment@users.noreply.github.com>
2021-05-27 10:59:15 +0200

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -10,21 +10,16 @@ namespace RunLocalPowershell.Models
     public class User
     {
 
-        [Required(ErrorMessage = "Firstname is required")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Lastname is required")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "LogonName is required")]
         public string LogonName { get; set; }
 
-        [Required(ErrorMessage = "DisplayName is required")]
         public string DisplayName { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
         public string EMail { get; set; }
-        public string PasswordResetNR { get; set; }
+        public string MobileNR { get; set; }
         public string TelephoneNR { get; set; }
         public string Description { get; set; }
         public string Company { get; set; }
@@ -33,11 +28,9 @@ namespace RunLocalPowershell.Models
         public string Department { get; set; }
         public string Office { get; set; }
 
-        [Required(ErrorMessage = "Manager is required")]
         public string Manager { get; set; }
-
-        [Required(ErrorMessage = "Container is required")]
-        public string Container { get; set; }
+        public int Accounttype { get set; }
+        
         public string Street { get; set; }
         public string City { get; set; }
         public string ZIP { get; set; }
@@ -47,10 +40,6 @@ namespace RunLocalPowershell.Models
         public DateTime? ExpirationDate { get; set; }
         public string License { get; set; }
 
-        [Required(ErrorMessage = "ExchangeServer is required")]
-        public string ExchangeServer { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
 
     }
